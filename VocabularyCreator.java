148ab64b1525cc2877b9951f2d71287147dc48a4
taiarima <tai.arima@colorado.edu>
2023-06-29 00:40:14 +0300

    Refactoring. Still in progress.

--- VocabularyCreator.java
+++ VocabularyCreator.java
@@ -1,31 +1,70 @@
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class VocabularyCreator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Provide the file name you would like to make a vocabulary list for: ");
+        System.out.print("Provide the file name you would like to make a vocabulary list for: ");
         String fileName = scanner.nextLine();
 
-        // Load text from the file here
         
-        String text = "한국말은 재미있는 언어예요";
+        String[] textFragments = loadTextFromFile(fileName);
 
-        System.out.println("Choose a name for your file");
-        String outputFile = scanner.nextLine();
+        if (textFragments != null) {
+            System.out.println("Choose a name for your file");
+            String outputFile = scanner.nextLine();
 
+            boolean success = processTextFragments(scanner, textFragments, outputFile);
+            if (success) {
+                System.out.println("Your vocabulary list has been successfully saved as " + outputFile + ".js");
+            } else {
+                System.out.println("An error occurred and your work could not be saved.");
+            }
+        } else {
+            System.out.println("Your text file was empty");
+        }
+
+        scanner.close();
+    }
+
+    private static String[] loadTextFromFile(String fileName) {
+        StringBuilder textBuilder = new StringBuilder();
+        try {
+            FileReader reader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                textBuilder.append(line).append(" ");
+            }
+
+            reader.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        String allText = textBuilder.toString().replaceAll("\\p{Punct}", "");
+        String[] textFragments = allText.split("\\s+");
+        return textFragments;
+    }
+
+    private static boolean processTextFragments(Scanner scanner, String[] textFragments, String outputFile) {
         try {
             PrintWriter writer = new PrintWriter(outputFile + ".js");
 
-            String[] fragments = text.split(" ");
-            for (String fragment : fragments) {
+            for (String fragment : textFragments) {
                 System.out.println("Text fragment: " + fragment + ". Split into multiple cards?");
                 String cardsInput = scanner.nextLine();
 
                 String[] cards;
                 if (cardsInput.isEmpty()) {
-                    cards = new String[] { fragment };
+                    cards = new String[]{fragment};
                 } else {
                     cards = cardsInput.split(" ");
                 }
@@ -36,17 +75,15 @@ public class VocabularyCreator {
                     System.out.println("Card " + card + " english: ");
                     String english = scanner.nextLine();
 
-                    // Here you write the card to the .js file
                     writer.println("{korean: \"" + (korean.isEmpty() ? card : korean) + "\", english: \"" + english + "\"}");
                 }
             }
 
             writer.close();
-            scanner.close();
-            System.out.println("Your vocabulary list has been successfully saved as " + outputFile + ".js");
+            return true;
         } catch (FileNotFoundException e) {
-            System.out.println("An error occurred and your work could not be saved.");
-            scanner.close();
+            e.printStackTrace();
+            return false;
         }
     }
 }
